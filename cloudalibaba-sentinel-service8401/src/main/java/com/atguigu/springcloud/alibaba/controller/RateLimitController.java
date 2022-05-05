0cfcad077bc9cec8f6a415001d613e741732809d
wuxinxin2020 <2252018672@qq.com>
2022-05-05 14:25:41 +0800

    init

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/RateLimitController.java
@@ -17,20 +17,20 @@ public class RateLimitController
 {
     @GetMapping("/byResource")
     @SentinelResource(value = "byResource",blockHandler = "handleException")
-    public CommonResult byResource()
+    public CommonResult<Payment> byResource()
     {
-        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
+        return new CommonResult<Payment>(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
     }
-    public CommonResult handleException(BlockException exception)
+    public CommonResult<String> handleException(BlockException exception)
     {
-        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+        return new CommonResult<String>(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
     }
 
     @GetMapping("/rateLimit/byUrl")
     @SentinelResource(value = "byUrl")
-    public CommonResult byUrl()
+    public CommonResult<Payment> byUrl()
     {
-        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+        return new CommonResult<Payment>(200,"按url限流测试OK",new Payment(2020L,"serial002"));
     }
 
 
@@ -38,8 +38,8 @@ public class RateLimitController
     @SentinelResource(value = "customerBlockHandler",
             blockHandlerClass = CustomerBlockHandler.class,
             blockHandler = "handlerException2")
-    public CommonResult customerBlockHandler()
+    public CommonResult<Payment> customerBlockHandler()
     {
-        return new CommonResult(200,"按客戶自定义",new Payment(2020L,"serial003"));
+        return new CommonResult<Payment>(200,"按客戶自定义",new Payment(2020L,"serial003"));
     }
 }
